ca8b4417396328c5c9ff34feee57cec59b18839a
obfrap <masterofdestruction@gmail.com>
2020-04-15 17:01:32 -0700

    Have removed header row and separated data into two lists

--- PyBank/main.py
+++ PyBank/main.py
@@ -8,6 +8,7 @@ import os
 csv_path = pathlib.Path('Resources/budget_data.csv')
 
 #Create lists to store the data
+my_list=[]
 profit_loss = []
 date_combined = []
 month = []
@@ -17,9 +18,20 @@ year = []
 with open(csv_path) as csv_file:
     csv_data = csv.reader(csv_file)
     
-    #Read Header row and id
+    #can skip header row and save if needed for later
     csv_header=next(csv_data)
-    # print(csv_header)
-    
-    for x in csv_data:
-        print(x=2)
+
+    #once skipped append values to separate lists
+    for row in csv_data:
+        date_combined.append(row[0])
+        profit_loss.append(int(row[1]))
+#print(date_combined)
+#print(profit_loss)
+
+#count total # of months
+tot_mon=len(date_combined)
+#print(tot_mon)
+net_tot=sum(profit_loss)
+print(net_tot)
+
+
