14033630ee597981d9714a8b685d819d00ededb9
obfrap <masterofdestruction@gmail.com>
2020-04-16 11:22:45 -0700

    Cleaned up code, removed print statements used for checks

--- PyBank/main.py
+++ PyBank/main.py
@@ -11,8 +11,7 @@ csv_path = pathlib.Path('Resources/budget_data.csv')
 my_list=[]
 profit_loss = []
 date_combined = []
-month = []
-year = []
+
 
 #Open csv file and read
 with open(csv_path) as csv_file:
@@ -25,25 +24,21 @@ with open(csv_path) as csv_file:
     for row in csv_data:
         date_combined.append(row[0])
         profit_loss.append(int(row[1]))
-#print(date_combined)
-#print(profit_loss)
 
-#count total # of months
+
+#count total # of months and other values requested
 tot_mon=len(date_combined)
-#print(tot_mon)
+
 net_tot=sum(profit_loss)
-#print(net_tot)
+
 avg_pl=round((net_tot/tot_mon),2)
-#print(avg_pl)
+
 gr_inc=int(max(profit_loss))
 gr_dec=int(min(profit_loss))
-#print(gr_inc)
-#print(gr_dec)
 
 #find index location for each value
 gr_inc_idx=profit_loss.index(gr_inc)
 gr_dec_idx=profit_loss.index(gr_dec)
-#print(gr_inc_idx)
 
 #Print it all out
 print("'''text")
@@ -55,3 +50,18 @@ print(f"Average Change: ${avg_pl}")
 print(f"Greatest Increase in Profits: {date_combined[gr_inc_idx]} (${profit_loss[gr_inc_idx]})")
 print(f"Greatest Decrease in Profits: {date_combined[gr_dec_idx]} (${profit_loss[gr_dec_idx]})")
 print("'''")
+
+#Create Output
+output_path = pathlib.Path('Analysis/Results.txt')
+
+with open(output_path, 'w') as output:
+    output.write("'''text \n")
+    output.write("Financial Analysis \n")
+    output.write("---------------------------- \n")
+    output.write(f"Total Months: {tot_mon} \n")
+    output.write(f"Total: ${net_tot} \n")
+    output.write(f"Average Change: ${avg_pl} \n")
+    output.write(f"Greatest Increase in Profits: {date_combined[gr_inc_idx]} (${profit_loss[gr_inc_idx]}) \n")
+    output.write(f"Greatest Decrease in Profits: {date_combined[gr_dec_idx]} (${profit_loss[gr_dec_idx]}) \n")
+    output.write("'''" )
+
