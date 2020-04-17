8043bcd67c174142d473e311b6b98916635c735e
obfrap <masterofdestruction@gmail.com>
2020-04-17 14:55:51 -0700

    now have added order to individual candidate list

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -12,7 +12,7 @@ csv_loc = pathlib.Path('Resources/election_data.csv')
 voter_id=[]
 county=[]
 candidate =[]
-
+can_list = []
 # Open csv file and read
 with open(csv_loc) as csv_file:
     csv_data = csv.reader(csv_file)
@@ -30,9 +30,20 @@ tot_votes_cast=len(voter_id)
 #print(tot_votes_cast)
 
 #set command will make new list of all unique inputs
+
 can_list = set(candidate)
-#print (can_list)
+can_or = sorted(can_list)
+print(can_or)
+# def unique(sequence):
+#     seen = set(candidate)
+#     return [x for x in sequence if not (x in seen or seen.add(x))]
+#     print(x)
 
-#create loop.  Use can_list position to search for unique names and create tallies
-
-
+#can_list=set(candidate)
+#print(can_list)
+# candidate_1 = candidate[0]
+# print(candidate_1)
+# for row in candidate:
+#     if row != str(candidate_1)
+#     candidate_2 = row
+#     print(candidate_2)
